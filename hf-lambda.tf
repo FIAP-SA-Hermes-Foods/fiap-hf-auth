83a6b4767db4f1ac57e48177cefb7d938f871e80
MarcosP <43271729+marcos-dev88@users.noreply.github.com>
2024-03-19 19:14:05 -0300

    Update hf-lambda.tf

--- hf-lambda.tf
+++ hf-lambda.tf
@@ -9,6 +9,6 @@ resource "aws_lambda_function" "hf_lambda" {
   timeout       = 10 // tempo limite em segundos
   memory_size   = 128 // tamanho da memória em MB
   filename      = "bootstrap.zip" // arquivo zipado com o código da função
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("bootstrap.zip")  
   role = "{{LAMBDA_EXEC_PERM}}"
 }
